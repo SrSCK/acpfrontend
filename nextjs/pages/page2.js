108053cf2ede207d2cd61d6f8192581e2653829b
Sorawat <152917361+SrSCK@users.noreply.github.com>
2024-09-16 14:35:25 +0700

    upload next.js

---
+++ nextjs/pages/page2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+
+export default function Test() {
+  return (<Box>
+    <Button variant="contained">HelloWorld!!!</Button>
+
+  </Box>);
+}
