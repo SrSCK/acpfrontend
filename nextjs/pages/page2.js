cf9c010bffd27979468e351a3a9605f278a6b519
Sorawat <66011232@kmitl.ac.th>
2024-09-16 14:42:03 +0700

    'new'

--- nextjs/pages/page2.js
+++ nextjs/pages/page2.js
@@ -3,7 +3,7 @@ import { Box, Button } from "@mui/material";
 
 export default function Test() {
   return (<Box>
-    <Button variant="contained">HelloWorld!!!</Button>
+    <Button variant="contained">HealkdsfjlloWorld!!!</Button>
 
   </Box>);
 }
